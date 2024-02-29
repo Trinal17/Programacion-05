7ade67469c3e88653646d4074f678c4fa8bf7062
ProfeMelola <melola@educa.madrid.org>
2024-02-29 13:35:07 +0100

    renombrado

--- EJERCICIOS/EXCEPCIONES/PhoneNumberException/src/RegisterClientApp.java
+++ EJERCICIOS/EXCEPCIONES/PhoneNumberException/src/RegisterClientApp.java
@@ -1,7 +1,7 @@
 
 import es.daw.poo2.model.PhoneNumberRegisterService;
 
-public class DarAltaClienteApp {
+public class RegisterClientApp {
     public static void main(String[] args) {
 
         // PENDIENTE COMPLETAR...
